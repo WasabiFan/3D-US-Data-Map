40f3bcdf376327529784a7b92b6686ef4508221b
WasabiFan <wasabifan@outlook.com>
2014-12-15 21:13:04 -0800

    Replace complicated grunt script with simple copy operation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,117 +1,13 @@
-var refInsertMarkers = ['<!----FILE LINKS-->', '<!----END FILE LINKS-->'];
-var fileLinks =
-'<script type="text/javascript">{DATAVARS}</script>\
-<script type="text/javascript">{CSVDATA}</script>\
-<script type="text/javascript">{SCRIPTS}</script>\
-<style>{STYLES}</style>'
-
-var filesToPublish = ['Index.html', 'Data/USStates.svg', 'Data/USCounties.svg', 'Data/UCR2012.csv', 'theme/*', 'theme/images/*'];
 
 module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        uglify: {
-            options: {
-                banner: '/*\n' +
-                ' * ' + '<%= pkg.name %>\n' +
-                ' * ' + 'v<%= pkg.version %>\n' +
-                ' * ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-                ' **/\n',
-                beautify: true
-            },
-
-            main: {
-                files: {
-                    'Build/Scripts.min.js':
-                        [
-                            'OrbitControls.js',
-                            'raphael.js',
-                            'Global.js',
-							'DataPlugins.js',
-							'Data.js',
-                            'Census.js',
-							'CrimeUCR.js',
-                            '3DScene.js',
-                            'Selection.js',
-                            'Script.js'
-                        ],
-                    'Build/Variables.min.js':
-                        [
-                            'Build/Variables.js'
-                        ]
-                }
-            }
-        },
-
         copy: {
             main: {
-                options: {
-                    process: function (content, srcpath) {
-                        var result = content.split('');
-
-                        var currString = result.join('');
-                        var start = currString.indexOf(refInsertMarkers[0]);
-                        var end = currString.indexOf(refInsertMarkers[1]) + refInsertMarkers[1].length;
-                        result.splice(start, end - start);
-
-                        var scripts = fileLinks;
-
-                        scripts = scripts.replace('{SCRIPTS}', grunt.file.read('Build/Scripts.min.js'));
-                        scripts = scripts.replace('{STYLES}', grunt.file.read('Build/Styles.min.css'));
-                        scripts = scripts.replace('{DATAVARS}', grunt.file.read('Build/Variables.min.js'));
-
-                        var escapedCountyData = grunt.file.read('Data/CountyData.txt').replace(/\n/g, '\\n');
-                        var escapedStateData = grunt.file.read('Data/StateData.csv').replace(/\r\n/g, '\\n');
-
-                        scripts = scripts.replace('{CSVDATA}', 'var preloadedCountyData = "' + escapedCountyData + '"; var preloadedStateData = "' + escapedStateData + '";');
-
-                        result.splice(start, 0, scripts);
-                        var splicedString = result.join('');
-
-                        return splicedString;
-                    }
-                },
-
-                files: [
-                  { expand: true, src: ['Index.html'], dest: 'Build' },
-                ]
-            },
-            data: {
-                src: 'Data/*',
+                src: '**',
                 dest: 'Build/',
-            },
-			theme: {
-                src: ['theme/*', 'theme/images/*'],
-                dest: 'Build/',
-            },
-        },
-
-        cssmin: {
-            minify: {
-                expand: true,
-                src: ["Styles.css"],
-                dest: 'Build/',
-                ext: '.min.css'
-            }
-        },
-
-        concat: {
-            main: {
-                options: {
-                    process: function (src, file) {
-                        return 'if(typeof preloadedInitProps == "undefined") {preloadedInitProps = {};}; preloadedInitProps["' + file + '"] = ' + src + ';\n';
-                    }
-                },
-
-                files: {
-                    'Build/Variables.js':
-                        [
-                            'Data/Variables2010acs5.js',
-                            'Data/Variables2010sf1.js'
-                        ]
-                },
             }
         },
 
@@ -120,7 +16,7 @@ module.exports = function (grunt) {
                 options: {
                     base: 'Build'
                 },
-                src: filesToPublish
+                src: '**'
             },
 			secondary: {
                 options: {
@@ -129,7 +25,7 @@ module.exports = function (grunt) {
 					add:true,
 					branch: 'master'
                 },
-                src: filesToPublish
+                src: '**'
             }
         }
 
@@ -142,9 +38,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-gh-pages');
 
     grunt.registerTask('default', function () {
-        grunt.task.run('concat')
-        grunt.task.run('uglify');
-        grunt.task.run('cssmin');
         grunt.task.run('copy');
     });
 
